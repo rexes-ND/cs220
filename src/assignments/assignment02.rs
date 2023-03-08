6da24429c46ebefc5486dcdc321e6453a7e35a06
Erkhes <erkhes.nyamsaikhan@gmail.com>
2023-03-09 03:51:42 +0900

    Finish assignment02

--- src/assignments/assignment02.rs
+++ src/assignments/assignment02.rs
@@ -13,36 +13,89 @@ const FAHRENHEIT_SCALE: f64 = 5.0 / 9.0;
 
 /// Converts Fahrenheit to Celsius temperature degree.
 pub fn fahrenheit_to_celsius(degree: f64) -> f64 {
-    todo!()
+    (degree - 32.0) / 1.8
 }
 
 /// Capitalizes English alphabets (leaving the other characters intact).
 pub fn capitalize(input: String) -> String {
-    todo!()
+    let mut res = String::new();
+    for c in input.chars() {
+        if c.is_ascii_alphabetic() {
+            res.push(c.to_ascii_uppercase());
+        } else {
+            res.push(c);
+        }
+    }
+    res
 }
 
 /// Returns the sum of the given array. (We assume the absence of integer overflow.)
 pub fn sum_array(input: &[u64]) -> u64 {
-    todo!()
+    let mut sum: u64 = 0;
+    for el in input {
+        sum += el;
+    }
+    sum
 }
 
 /// Given a non-negative integer, say `n`, return the smallest integer of the form `3^m` that's greater than or equal to `n`.
 ///
 /// For instance, up3(6) = 9, up3(9) = 9, up3(10) = 27. (We assume the absence of integer overflow.)
 pub fn up3(n: u64) -> u64 {
-    todo!()
+    let mut res: u64 = 1;
+    while res < n {
+        res *= 3;
+    }
+    res
 }
 
 /// Returns the greatest common divisor (GCD) of two non-negative integers. (We assume the absence of integer overflow.)
 pub fn gcd(lhs: u64, rhs: u64) -> u64 {
-    todo!()
+    if lhs == 0 {
+        return rhs;
+    }
+    if rhs == 0 {
+        return lhs;
+    }
+    if lhs == 1 || rhs == 1 {
+        return 1;
+    }
+    if lhs > rhs {
+        if lhs % rhs == 0 {
+            return rhs;
+        }
+        gcd(lhs % rhs, rhs)
+    } else {
+        if rhs % lhs == 0 {
+            return lhs;
+        }
+        gcd(lhs, rhs % lhs)
+    }
 }
 
 /// Returns the array of nC0, nC1, nC2, ..., nCn, where nCk = n! / (k! * (n-k)!). (We assume the absence of integer overflow.)
 ///
 /// Consult <https://en.wikipedia.org/wiki/Pascal%27s_triangle> for computation of binomial coefficients without integer overflow.
 pub fn chooses(n: u64) -> Vec<u64> {
-    todo!()
+    let mut vec = Vec::new();
+    let mut i: u64 = 1;
+    vec.push(1);
+    if n == 0 {
+        return vec;
+    }
+    vec.push(1);
+    while i < n {
+        i += 1;
+        let mut nextvec = Vec::new();
+        nextvec.push(1);
+        let iu: usize = i.try_into().unwrap();
+        for j in 0..(iu - 1) {
+            nextvec.push(vec[j] + vec[j + 1]);
+        }
+        nextvec.push(1);
+        vec = nextvec;
+    }
+    vec
 }
 
 /// Returns the "zip" of two vectors.
@@ -50,7 +103,12 @@ pub fn chooses(n: u64) -> Vec<u64> {
 /// For instance, `zip(vec![1, 2, 3], vec![4, 5])` equals to `vec![(1, 4), (2, 5)]`.
 /// Here, `3` is ignored because it doesn't have a partner.
 pub fn zip(lhs: Vec<u64>, rhs: Vec<u64>) -> Vec<(u64, u64)> {
-    todo!()
+    let minlen = (lhs.len()).min(rhs.len());
+    let mut res = Vec::new();
+    for i in 0..minlen {
+        res.push((lhs[i], rhs[i]));
+    }
+    res
 }
 
 /// 2x2 matrix of the following configuration:
@@ -91,7 +149,12 @@ impl Mul<Mat2> for Mat2 {
     type Output = Mat2;
 
     fn mul(self, rhs: Mat2) -> Self::Output {
-        todo!()
+        Self::Output {
+            a: self.a * rhs.a + self.b * rhs.c,
+            b: self.a * rhs.b + self.b * rhs.d,
+            c: self.c * rhs.a + self.d * rhs.c,
+            d: self.c * rhs.b + self.d * rhs.d,
+        }
     }
 }
 
@@ -99,21 +162,31 @@ impl Mul<Vec2> for Mat2 {
     type Output = Vec2;
 
     fn mul(self, rhs: Vec2) -> Self::Output {
-        todo!()
+        Self::Output {
+            a: self.a * rhs.a + self.b * rhs.b,
+            b: self.c * rhs.a + self.d * rhs.b,
+        }
     }
 }
 
 impl Mat2 {
     /// Calculates the power of matrix.
     fn power(self, power: u64) -> Mat2 {
-        todo!()
+        if power == 0 {
+            return Mat2::new();
+        }
+        let half = self.power(power / 2);
+        if power % 2 == 1 {
+            return self * half * half;
+        }
+        half * half
     }
 }
 
 impl Vec2 {
     /// Gets the upper value of vector.
     fn get_upper(self) -> u64 {
-        todo!()
+        self.a
     }
 }
 
@@ -139,5 +212,112 @@ pub fn fibonacci(n: u64) -> u64 {
 ///
 /// Hint: Google the song title for lyrics and look at the test code for the expected result.
 pub fn twelve_days_of_christmas_lyrics() -> String {
-    todo!()
+    let mut res = String::new();
+    res.push_str(
+        "On the first day of Christmas, my true love sent to me
+A partridge in a pear tree
+
+On the second day of Christmas, my true love sent to me
+Two turtledoves
+And a partridge in a pear tree
+
+On the third day of Christmas, my true love sent to me
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the fourth day of Christmas, my true love sent to me
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the fifth day of Christmas, my true love sent to me
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the sixth day of Christmas, my true love sent to me
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the seventh day of Christmas, my true love sent to me
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the eighth day of Christmas, my true love sent to me
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the ninth day of Christmas, my true love sent to me
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the tenth day of Christmas, my true love sent to me
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the eleventh day of Christmas, my true love sent to me
+I sent eleven pipers piping
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the twelfth day of Christmas, my true love sent to me
+Twelve drummers drumming
+Eleven pipers piping
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+And a partridge in a pear tree
+",
+    );
+    res
 }
